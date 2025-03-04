948b6830368c01f6e8a2bf7ae783b669d43431ef
anshu638 <anshu@nextsavy.com>
2025-03-04 12:51:22 +0530

    Structure preped
    Folders added, service defined, bloc and cache methods added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:challenges/service_locator.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await initializeDependencies();
   runApp(const MyApp());
 }
 
